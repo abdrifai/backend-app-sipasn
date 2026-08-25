41b1af7ca95a876ccb801e4aff933d8e60911261
abd.rifai <rhifay@gmail.com>
2026-08-26 00:51:32 +0800

    fix: perbaiki nama import middleware autentikasi dari verifyToken ke authenticate pada pensiun.route.js

--- src/modules/pensiun/pensiun.route.js
+++ src/modules/pensiun/pensiun.route.js
@@ -3,11 +3,11 @@ import * as pensiunController from "./pensiun.controller.js";
 import { validate } from "../../middlewares/validate.middleware.js";
 import { createPensiunSchema } from "./pensiun.validation.js";
 import { uploadDokumenSK } from "../../middlewares/upload.middleware.js";
-import { verifyToken } from "../../middlewares/auth.middleware.js";
+import { authenticate } from "../../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.use(verifyToken);
+router.use(authenticate);
 
 router.get("/kedudukan-options", pensiunController.getKedudukanOptions);
 router.get("/", pensiunController.getAllPensiun);
